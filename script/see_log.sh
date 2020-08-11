ef06c38ed9b6abba314793bb55b2438b0715fc0c
hyliang96 <hyliang96@hotmail.com>
2020-08-11 20:12:53 +0800

    add docker-compose log

--- script/see_log.sh
+++ script/see_log.sh
@@ -8,6 +8,7 @@ cd "$scheme_root"
 
 
 log_list=(
+    "docker-compose-log"
     "$scheme_root/v2ray/log/access.log"
     "$scheme_root/v2ray/log/error.log"
     "$scheme_root/nginx/log/access.log"
@@ -17,6 +18,7 @@ log_list=(
 )
 
 log_prompt_list=(
+    '服务端 docker-compose 的 log'
     '服务端 v2ray access log'
     '服务端 v2ray error log'
     '服务端 nginx access log'
@@ -30,12 +32,16 @@ end_index=$(( ${#log_prompt_list[@]} - 1))
 end_index_minux_1=$(( ${end_index} - 1 ))
 
 while true; do
+    echo
     for ((i=0; i<=${end_index}; i++ )); do
         echo "[$i] ${log_prompt_list[$i]}"
     done
 
     index=$(bash -c "read -n 1 -p '选择log的编号? [0-${end_index}] ' c; echo \$c"); echo
-    if [[ "$index" =~ ^[0-${end_index_minux_1}]$ ]]; then
+    if [ "${log_list[$index]}" = 'docker-compose-log' ]; then
+        cd "$scheme_root"
+        docker-compose logs -t -f
+    elif [[ "$index" =~ ^[0-${end_index_minux_1}]$ ]]; then
         echo "less ${log_list[$index]}"
         less "${log_list[$index]}"
     elif [ "$index" = "${end_index}" ]; then
@@ -43,6 +49,5 @@ while true; do
     else
         echo "请输入 0-${end_index}"
     fi
-    echo
 done
 
